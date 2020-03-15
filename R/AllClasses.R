fc82b470f7f08cf4c78acf92efc6986aab7a903d
drighelli <d.righelli@gmail.com>
2020-03-15 17:30:10 +0100

    checking visium coordinates

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -118,7 +118,7 @@ VisiumExperiment <- function(..., spatialCoords=data.frame(),
 {
     ve <- new("VisiumExperiment", se)
     
-    .Object <- checkSpatialCoords(ve, spatialCoords)
+    .Object <- checkVisumSpatialCoords(ve, spatialCoords)
     .Object <- addScaleFactors(.Object, scaleFactors)
     return(.Object)
 }
