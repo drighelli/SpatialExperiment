be0b2249e88b5b5d70e4cc5a0b72e99c1553ac0e
drighelli <d.righelli@gmail.com>
2020-07-09 19:21:08 +0200

    bumping to version 0.99.1

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -80,7 +80,7 @@ SpatialExperiment <- function(..., spatialCoords=data.frame(),
     return(.sce_to_se(sce, spatialCoords=spatialCoords, cellColID=cellColID))
 }
 
-#' @importClassesFrom S4Vectors DataFrame
+#' @importClassesFrom S4Vectors DataFrame disabeValidity
 #' @importFrom S4Vectors DataFrame isEmpty
 #' @importFrom methods new
 .sce_to_se <- function(sce, spatialCoords=DataFrame(), cellColID="Cell_ID") 
