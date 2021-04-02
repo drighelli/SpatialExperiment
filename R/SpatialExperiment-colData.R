ad66aadd0cc908ec41a4fa7fbffda2f967f698fc
HelenaLC <helena@crowells.eu>
2021-04-02 16:17:55 +0200

    protect sample_id & spatialDataNames

--- R/SpatialExperiment-colData.R
+++ R/SpatialExperiment-colData.R
@@ -39,32 +39,48 @@ setReplaceMethod("colData",
         colData(se) <- value
         new <- colData(se)
         
-        # check that 'sample_id's remain valid & update 'imgData' accordingly
-        ns_old <- length(sids_old <- unique(old$sample_id))
-        ns_new <- length(sids_new <- unique(new$sample_id))
-        if (ns_old != ns_new) {
-            warning(sprintf(
-                "Number of unique 'sample_id's is %s, but %s %s provided.\n",
-                ns_old, ns_new, ifelse(ns_new > 1, "were", "was")))
-        } else if (sum(table(old$sample_id, new$sample_id) != 0) != ns_old) {
-            warning("New 'sample_id's must map uniquely")
-        } else if (!is.null(imgData(x))) {
-            m <- match(imgData(x)$sample_id, sids_old)
-            imgData(x)$sample_id <- sids_new[m]
+        if (!is.null(new$sample_id)) {
+            # check that 'sample_id's remain valid & update 'imgData' accordingly
+            ns_old <- length(sids_old <- unique(old$sample_id))
+            ns_new <- length(sids_new <- unique(new$sample_id))
+            if (ns_old != ns_new) {
+                stop(sprintf(
+                    "Number of unique 'sample_id's is %s, but %s %s provided.\n",
+                    ns_old, ns_new, ifelse(ns_new > 1, "were", "was")))
+            } else if (sum(table(old$sample_id, new$sample_id) != 0) != ns_old) {
+                stop("New 'sample_id's must map uniquely")
+            } else if (!is.null(imgData(x))) {
+                m <- match(imgData(x)$sample_id, sids_old)
+                imgData(x)$sample_id <- sids_new[m]
+            }    
+        } else {
+            # if none provided, retain original sample_id field
+            value$sample_id <- old$sample_id
         }
+
+        # protect spatialData from being replaced
+        spd <- spatialData(x, 
+            spatialCoords=FALSE, 
+            colData=FALSE)
+        value <- cbind(value, spd)
         BiocGenerics:::replaceSlots(x, colData=value, check=FALSE)
     }
 )
 
 #' @rdname SpatialExperiment-colData
-#' @importFrom S4Vectors DataFrame
 #' @importFrom SummarizedExperiment colData colData<-
 #' @export
 setReplaceMethod("colData",
     c("SpatialExperiment", "NULL"),
     function(x, value) {
-        warning("Dropping colData could break",
-            " imgData and spatialData functionality.")
-        BiocGenerics:::replaceSlots(x, colData=value, check=FALSE)
+        # replacement by NULL keeps 
+        # sample_id & spatialDataNames
+        ids <- colData(x)["sample_id"]
+        spd <- spatialData(x, 
+            spatialCoords=FALSE, 
+            colData=FALSE)
+        value <- cbind(ids, spd)
+        colData(x) <- value
+        return(x)
     }
 )
